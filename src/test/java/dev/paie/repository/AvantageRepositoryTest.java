aeaba610216c7684b006bf3e311865e26935b555
Mélodie MAUBOIS <maubois.melodie@orange.fr>
2017-12-19 18:09:25 +0100

    spring jpa tjr en cours

--- src/test/java/dev/paie/repository/AvantageRepositoryTest.java
+++ src/test/java/dev/paie/repository/AvantageRepositoryTest.java
@@ -1,5 +1,7 @@
 package dev.paie.repository;
 
+import java.math.BigDecimal;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import dev.paie.config.JeuxDeDonneesConfig;
 import dev.paie.config.ServicesConfig;
-import dev.paie.entite.Cotisation;
+import dev.paie.entite.Avantage;
 
 //Sélection des classes de configuration Spring à utiliser lors du test
 @ContextConfiguration(classes = { ServicesConfig.class, JeuxDeDonneesConfig.class })
@@ -23,9 +25,18 @@ public class AvantageRepositoryTest {
 	@Test
 	public void test_sauvegarder_lister_mettre_a_jour() {
 	// TODO sauvegarder un nouvel avantage
-		
+		Avantage a = new Avantage();
+		a.setCode("AV01");
+		a.setMontant(new BigDecimal("100"));
+		a.setNom("Avantage 1");
+		avantageRepository.save(a);
 	// TODO vérifier qu'il est possible de récupérer le nouvel avantage via la méthode findOne
+		avantageRepository.findOne(1);
 	// TODO modifier un avantage
+		Avantage a2 = new Avantage();
+		a.setCode("AV01");
+		a.setMontant(new BigDecimal("100"));
+		a.setNom("Avantage 1");
 	// TODO vérifier que les modifications sont bien prises en compte via la méthode findOne
 	}
 }
